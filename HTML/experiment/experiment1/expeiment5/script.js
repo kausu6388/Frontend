b93eb4afde2d842ec9fe6479d7589b641af3fe4f
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-10-29 09:12:25 +0530

    new file added

---
+++ HTML/experiment/experiment1/expeiment5/script.js
@@ -0,0 +1,7 @@
+// const username = prompt("mr. kaushik");
+// alert(`hello ${username} welcome to my website`);
+let age = prompt("enter age:");
+
+age = Number(age); 
+
+age >= 18 ?alert("Tum vote dene ke liye eligible ho!")  : alert("Tum abhi vote dene ke liye eligible nahi ho!");
